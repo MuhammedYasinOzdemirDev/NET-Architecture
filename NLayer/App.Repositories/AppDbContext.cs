903dc78ae6b01d8edb6e89e3a2b8b1e2455f864a
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2025-10-19 17:34:09 +0300

    Add AppDbContext with EF Core configuration support
    Introduces the `AppDbContext` class using Entity Framework Core. Includes a `Products` DbSet and applies all configurations from the current assembly by default. This setup streamlines the database context and configuration management.

---
+++ NLayer/App.Repositories/AppDbContext.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using App.Repositories.Configuration;
+using App.Repositories.Entity;
+using Microsoft.EntityFrameworkCore;
+
+namespace App.Repositories;
+
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) //dotnet 8 beraber gelen ozellik
+{
+    public DbSet<Product> Products { get; set; } = default!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        //icerisine ayri classlar configration ile yapilmali
+        // modelBuilder.ApplyConfiguration(new ProductConfiguration());// bu product tarafi icin olur ama bunun yerine
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly
+            .GetExecutingAssembly()); //Bu kod mevcut calisan repository dizininde yer alan tum configrasyonlari yukler
+        base.OnModelCreating(modelBuilder);
+    }
+}
