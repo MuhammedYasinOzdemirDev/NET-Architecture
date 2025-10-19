3d8eae7c891dbe4e44df9a0881a5ef70c579b50e
Muhammed Yasin Özdemir <94251353+MuhammedYasinOzdemirDev@users.noreply.github.com>
2025-10-19 17:34:03 +0300

    Add ProductConfiguration for EF Core entity mapping
    Introduced `ProductConfiguration` class to configure the `Product` entity in EF Core using the Fluent API. This ensures the database schema aligns with required properties, such as primary keys, constraints, and data types, while keeping the entity models clean.

---
+++ NLayer/App.Repositories/Configuration/ProductConfiguration.cs
@@ -0,0 +1,17 @@
+using App.Repositories.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace App.Repositories.Configuration;
+
+public class ProductConfiguration:IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.HasKey(x => x.Id);//Primary Key
+        builder.Property(x => x.Name).IsRequired().HasMaxLength(150);
+        builder.Property(x => x.Price).HasColumnType("decimal(18,2)");
+        builder.Property(x => x.Stock).IsRequired();
+        //Not Modeller icinde attirubute olarak ornek [Required] olmamali modeller yalin olmali
+    }
+}
